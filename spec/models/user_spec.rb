f8538fa9323b5e6cdbce984b8b4fdfb1a2c52ef4
Fippiy <profier4@gmail.com>
2019-01-04 20:24:54 +0900

    Edit spec/models/user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 describe User do
   describe '#create' do
     it "is invalid without a nickname" do
-      user = User.new(nickname: "", email: "", password:"00000000", password_confirmation: "00000000")
+      # user = User.new(nickname: "", email: "", password:"00000000", password_confirmation: "00000000")
+      # user = FactoryGirl.build(:user)
+      user = build(:user, nickname: "")
       user.valid?
       expect(user.errors[:nickname]).to include("can't be blank")
-      expect(user.errors[:email]).to include("can't be blank")
     end
   end
 end

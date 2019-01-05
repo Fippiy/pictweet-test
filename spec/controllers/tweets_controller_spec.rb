0e8b8579ddf36e5988dab70d4bfc2760b08eed3a
Fippiy <profier4@gmail.com>
2019-01-05 13:39:10 +0900

    Edit tweets_controller_spec.rb

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -20,4 +20,15 @@ describe TweetsController, type: :controller do
       expect(response).to render_template :edit
     end
   end
+
+  describe 'GET #index' do
+    it "populates an array of tweets ordered by created_at DESC" do
+      tweets = create_list(:tweet, 3)
+      get :index
+      expect(assigns(:tweets)).to match(tweets)
+    end
+
+    it "renders the :index template" do
+    end
+  end
 end

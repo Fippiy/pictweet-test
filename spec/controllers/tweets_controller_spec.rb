f82b8d914fbefcc051e04ef2242018299bd2f5f5
Fippiy <profier4@gmail.com>
2019-01-05 13:13:02 +0900

    Edit spec/controllers/tweets_controller_spec.rb

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -7,4 +7,17 @@ describe TweetsController, type: :controller do
       expect(response).to render_template :new
     end
   end
+  describe 'GET #edit' do
+    it "assigns the requested contact to @tweet" do
+      tweet = create(:tweet)
+      get :edit, params: {id: tweet}
+      expect(assigns(:tweet)).to eq tweet
+    end
+
+    it "renders the :edit template" do
+      tweet = create(:tweet)
+      get :edit, params: {id: tweet}
+      expect(response).to render_template :edit
+    end
+  end
 end
